9ef4a6ab85718ddd360654c740058daf06ff676d
Phuoc Truong <phuoc.truong@pizzahut.io>
2020-01-09 10:47:32 +0700

    Add update route

--- nodejs/complete-node/task-manager/src/index.js
+++ nodejs/complete-node/task-manager/src/index.js
@@ -62,6 +62,26 @@ app.get('/users/:id', async (req, res) => {
     // })
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ "error": "Invalid updates"})
+    }
+    
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -114,6 +134,26 @@ app.get('/tasks/:id', async (req, res) => {
     // }).catch((e) => {
     //     res.status(500).send(e)
     // })
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowUpdateField = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowUpdateField.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ "error": "Invalid updates"})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+        if (!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
 app.listen(port, () => {
